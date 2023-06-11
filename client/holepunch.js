a53c54ea07e9a31cfda313966933b9028af95cc9
Ruulul <vamjunior01@gmail.com>
2023-06-10 22:49:05 -0300

    use autobase from github for latest resources

--- client/holepunch.js
+++ client/holepunch.js
@@ -5,14 +5,14 @@ console.log(db);
 
 
 
-// const doc = await db.collection('test').insert({text: 'Hi there'})
-// console.log(doc );
-//
-//
-// for await (let doc of db.collection('test').find({
-//     // clout: {
-//     //  $gt: 9000
-//     // },
-//    })) {
-//     console.log(doc)
-//    }
+const doc = await db.collection('test').insert({text: 'Hi there'})
+console.log(doc );
+
+
+for await (let doc of db.collection('test').find({
+    // clout: {
+    //  $gt: 9000
+    // },
+   })) {
+    console.log(doc)
+   }
