db100b8f2a0cfde1f4aeb6b71ca489ccc0259bb9
jermsam <ssali@jitpomi.com>
2023-06-10 20:27:31 -0600

    videos added

--- client/main.js
+++ client/main.js
@@ -1,26 +1,17 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-import {} from './holepunch';
+import {init} from './video.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+     <div id="videos" class="grid grid-cols-2 gap-2">
+      <video id="user-1" autoplay playsinline class="video-player"></video>
+      <video id="user-2" autoplay playsinline class="video-player"></video>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+ await init(
+  document.querySelector('#user-1'),
+  document.querySelector('#user-2')
+)
+
---
+++ client/video.js
@@ -0,0 +1,47 @@
+import db from './db';
+
+
+// console.log(db);
+
+
+
+// const doc = await db.collection('test').insert({text: 'Hi there'})
+// console.log(doc );
+//
+//
+// for await (let doc of db.collection('test').find({
+//     // clout: {
+//     //  $gt: 9000
+//     // },
+//    })) {
+//     console.log(doc)
+//    }
+
+// const videosCollection = db.collection('videos')
+
+export async function init(user1HTMLVideoElement, user2HTMLVideoElement) {
+  if(user1HTMLVideoElement && user2HTMLVideoElement) {
+    let localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    console.log(localStream);
+    user1HTMLVideoElement.srcObject = localStream;
+    
+    const stream = await db.collection('videos').insert({text: 'Hi there'})
+    console.log(stream);
+    
+    await createOffer(user2HTMLVideoElement)
+  }
+}
+
+export  async function createOffer(user2HTMLVideoElement) {
+  for await (let stream of db.collection('videos').find({
+    // clout: {
+    //  $gt: 9000
+    // },
+  })) {
+    console.log(stream)
+    let remoteStream = new MediaStream()
+    console.log(remoteStream);
+    user2HTMLVideoElement.srcObject = remoteStream;
+  }
+  
+}
